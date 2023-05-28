6e76799c5210564df68e519602c988de6e015008
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-05-28 21:01:54 +0530

    Create  Minimum Cost to Cut a Stick.java

---
+++ Minimum Cost to Cut a Stick.java
@@ -0,0 +1,35 @@
+Given a wooden stick of length n units. The stick is labelled from 0 to n. For example, a stick of length 6 is labelled as follows:
+
+
+Given an integer array cuts where cuts[i] denotes a position you should perform a cut at.
+
+You should perform the cuts in order, you can change the order of the cuts as you wish.
+
+The cost of one cut is the length of the stick to be cut, the total cost is the sum of costs of all cuts. When you cut a stick, it will be split into two smaller sticks (i.e. the sum of their lengths is the length of the stick before the cut). Please refer to the first example for a better explanation.
+
+Return the minimum total cost of the cuts.
+  Solution:
+import java.util.Arrays;
+
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        Arrays.sort(cuts);
+        int m = cuts.length;
+        int[][] dp = new int[m + 2][m + 2];
+
+        for (int l = 2; l <= m + 1; l++) {
+            for (int i = 0; i + l <= m + 1; i++) {
+                int j = i + l;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+                }
+                int left = (i == 0) ? 0 : cuts[i - 1];
+                int right = (j == m + 1) ? n : cuts[j - 1];
+                dp[i][j] += right - left;
+            }
+        }
+
+        return dp[0][m + 1];
+    }
+}
