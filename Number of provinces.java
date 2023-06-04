0af2b91d0d674cd702a0539bda8893f7451c09f8
Harsh kumar Agrawal <103424924+harsh-agrwal@users.noreply.github.com>
2023-06-04 19:22:35 +0530

    Create Number of provinces.java

---
+++ Number of provinces.java
@@ -0,0 +1,33 @@
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+  Solution:
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int provinces = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                provinces++;
+                dfs(isConnected, visited, i);
+            }
+        }
+
+        return provinces;
+    }
+
+    private void dfs(int[][] isConnected, boolean[] visited, int node) {
+        visited[node] = true;
+        for (int neighbor = 0; neighbor < isConnected.length; neighbor++) {
+            if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
+                dfs(isConnected, visited, neighbor);
+            }
+        }
+    }
+}
